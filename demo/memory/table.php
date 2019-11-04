237d005f03a322e417a248577929fb760726fbf8
panwei <1377853927@qq.com>
2019-11-04 16:31:51 +0800

    swoole 内存table

--- demo/memory/table.php
+++ demo/memory/table.php
@@ -14,7 +14,24 @@ $table->column('age', $table::TYPE_INT, 3);
 // 创建表
 $table->create();
 // 新增数据
-$table->set('panwei', ['id' => 1, 'name' => 'panwei', 'age' => 25]);
-
+$table->set('pw', ['id' => 1, 'name' => 'panwei', 'age' => 25]);
 // 读取数据
-print_r($table->get('panwei'));
+print_r($table->get('pw'));
+
+// 另一种方式
+$table['pw2'] = [
+    'id' => 2,
+    'name' => 'panwei2',
+    'age' => 24
+];
+
+$table->incr('pw2','age',100);
+print_r($table['pw2']);
+
+$table->decr('pw2','age',50 );
+print_r($table['pw2']);
+
+echo 'delete-start'.PHP_EOL;
+$table->del('pw2');
+echo 'delete-end'.PHP_EOL;
+print_r($table['pw2']);
